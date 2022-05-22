3d232ce6ce30765c9ad00a9c6a891afd033a0ab1
Bernie Petters <bp4924@gmail.com>
2022-05-22 10:19:13 -0400

    Update app.js
    fixed tagName line 27

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ for (i = 0; i < close.length; i++) {
 var list = document.querySelector('ul');
 
 list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'li') {
+  if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle("checked");
   };
 }, false);
