67d8678427cb1c41f8a101c2371cb7a9493c90ca
Bernie Petters <bp4924@gmail.com>
2022-05-23 09:22:15 -0400

    Add files via upload
    Added function call for enter event on text box

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var myNodeList = document.getElementsByTagName('LI');
-
 var i;
 
 for (i=0; i<myNodeList.length; i++) {
@@ -12,22 +11,30 @@ for (i=0; i<myNodeList.length; i++) {
 
 // click on a close button to hide items on the current list
 var close = document.getElementsByClassName('close');
-//var i;
-
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = 'none'
-  }
-}
+  };
+};
 
 var list = document.querySelector('ul');
-
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle("checked");
   };
 }, false);
+
+// add todo item on enter (no click required)
+document.getElementById('item').addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.key === 'Enter') {
+    newElement();
+  };
+  if (event.key === 'NumpadEnter') {
+    newElement();
+  };
+});
 
 function newElement() {
   var li = document.createElement('li');
@@ -39,13 +46,11 @@ function newElement() {
     alert ('This field cannot be empty');
   } else {
     document.getElementById('list').appendChild(li);
-  }
-  document.getElementById('item').value = ''; // clear input box
+  };
 
+  document.getElementById('item').value = ''; // clear input box
   span = document.createElement('span');
   txt = document.createTextNode('\u00D7');
-//  var span = document.createElement('span');
-//  var txt = document.createTextNode('\u00D7');
   span.className = 'close';
   span.appendChild(txt);
   li.appendChild(span);
